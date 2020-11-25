40428c2f029c287202c73ae396875968bd1c0de8
Jonas Brito <jonasexplore@gmail.com>
2020-11-25 16:11:43 -0300

    Encapsulamento e interface.

---
+++ exercicios/06/index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <h1>Projeto Controle Remoto</h1>
+    <pre>
+    <?php
+        require_once 'ControleRemoto.php';
+
+        $c = new ControleRemoto();
+
+        $c->ligar();
+        $c->maisVolume();
+        $c->abrirMenu();
+
+    ?>
+    </pre>
+</body>
+
+</html>
